f1a30061cee14bbb8146c9034273df187d7b0c56
gadapu <31911101+gadapu@users.noreply.github.com>
2019-06-26 09:57:44 +0530

    change userdata
    terraform

--- project.tf
+++ project.tf
@@ -3,7 +3,7 @@ data "aws_ami" "image" {
   most_recent = true
   filter {
     name   = "name"
-    values = ["packer-linux-aws-demo**"]
+    values = ["packer-linux-aws-demo*"]
   }
   filter {
     name   = "virtualization-type"
@@ -16,9 +16,7 @@ data "aws_ami" "image" {
 
 
 provider "aws" {
-  access_key = "${var.aws_access_key}"
-  secret_key = "${var.aws_secret_key}"
-  region     = "ap-south-1"
+  region     = "us-east-1"
 }
 
 resource "aws_instance" "example" {
@@ -26,11 +24,20 @@ resource "aws_instance" "example" {
  #ami            = "ami-0cc96feef8c6bbff3"
   instance_type  = "t2.micro"
   key_name       = "vakratunda"
-  #security_groups = ["jenkins-sg"]
+  security_groups = ["jenkins-sg"]
   user_data = <<-EOF
                 #!/bin/bash
                 sudo yum update -y
-                echo "Deployed via Terraform" > /var/www/html/index.html
+                mkdir app
+                chmod 777 app
+                cd app
+                git clone -b devlop https://github.com/chabhilashraju/tutor.git
+                chmod 777 tutor
+                cd tutor
+                npm install
+                export PATH=$PATH:/root/.npm-global/bin
+                service nginx restart
+                ng serve --host=0.0.0.0 --disable-host-check
         EOF
 
   tags {
